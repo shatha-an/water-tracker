6e6b62ba00a2eb209e5acedee2abaef8dd686e3e
shatha-an <shada-2013@hotmail.com>
2023-10-05 12:27:44 +0300

    Initial Commit

---
+++ water tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  water tracker
+//
+//  Created by Shatha Ajjaj on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ water tracker/water_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_trackerApp.swift
+//  water tracker
+//
+//  Created by Shatha Ajjaj on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct water_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
